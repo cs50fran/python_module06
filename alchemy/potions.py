6a3165c2ce19225d01929f1281e7cb882365952a
Francisco Dinis Da Fonseca Pedras Ribeiro <fdinis-d@c1r1s11.42lisboa.com>
2026-03-10 20:27:00 +0000

    v2

--- alchemy/potions.py
+++ alchemy/potions.py
@@ -1,14 +1,28 @@
-def healing_potion(fire_result, water_result):
-    return f"Healing potion brewed with {fire_result} and {water_result}"
+def healing_potion() -> str:
+    from .elements import create_fire, create_water
+    fire = create_fire()
+    water = create_water()
+    return f"Healing potion brewed with {fire} and {water}"
 
 
-def strength_potion(earth_result, fire_result):
-    return f"Strength potion brewed with {earth_result} and {fire_result}"
+def strength_potion() -> str:
+    from .elements import create_earth, create_fire
+    fire = create_fire()
+    earth = create_earth()
+    return f"Strength potion brewed with {earth} and {fire}"
 
 
-def invisibility_potion(air_result, water_result):
-    return f"Invisibility potion brewed with {air_result} and {water_result}"
+def invisibility_potion() -> str:
+    from .elements import create_air, create_water
+    air = create_air()
+    water = create_water()
+    return f"Invisibility potion brewed with {air} and {water}"
 
 
-def wisdom_potion(all_four_results):
-    return f"Wisdom potion brewed with all elements: {all_four_results}"
+def wisdom_potion() -> str:
+    from .elements import create_earth, create_air, create_fire, create_water
+    all_elements = (f"{create_fire()}, "
+                    f"{create_water()}, "
+                    f"{create_earth()}, "
+                    f"{create_air()}")
+    return f"Wisdom potion brewed with all elements: {all_elements}"
